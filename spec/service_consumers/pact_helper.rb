4b2d147d99ef93edf516fed23ec23253b3d09da3
Eugene Perov <evgenynikolaevich3@gmail.com>
2021-01-15 17:54:48 +0100

    Initialize

---
+++ spec/service_consumers/pact_helper.rb
@@ -0,0 +1,15 @@
+# In spec/service_consumers/pact_helper.rb
+
+require 'pact/provider/rspec'
+
+Pact.service_provider "Animal Service" do
+
+  honours_pact_with 'Zoo App' do
+
+    # This example points to a local file, however, on a real project with a continuous
+    # integration box, you would use a [Pact Broker](https://github.com/pact-foundation/pact_broker) or publish your pacts as artifacts,
+    # and point the pact_uri to the pact published by the last successful build.
+
+    pact_uri '../zoo-app/spec/pacts/zoo_app-animal_service.json'
+  end
+end
